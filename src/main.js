0a248b7c64de607767312ca7f803a54aeffb9338
黄晨 <18136225579@163.com>
2019-04-12 14:22:06 +0800

    feature: add less source

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 import router from './router'
 import store from './store'
+import api from './api'
 import i18n from './assets/lang/i18n'
-import 'ant-design-vue/dist/antd.css'
 import './permission'
+import 'ant-design-vue/dist/antd.less'
+import './assets/styles/index.less'
 
 Vue.config.productionTip = true
+
+Vue.use(api)
 
 Vue.use(Antd, {
   i18n: (key, value) => i18n.t(key, value)
